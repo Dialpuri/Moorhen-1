ddaa9187a4b3069813b9c93d8eb3c9ee51d5ad10
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-11-28 21:28:33 +0000

    Added sails first type

---
+++ baby-gru/src/types/sails.d.ts
@@ -0,0 +1,11 @@
+import { emscriptem } from "./emscriptem";
+
+export namespace sails {
+    type SiteResult = {
+        chain: str
+        seqid: str
+        name: str
+        type: str
+        key: str
+    }
+}
